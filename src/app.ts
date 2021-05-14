ed4fab5572ad143b0df9ec23d37b879bbf0073b3
Eduardo Canal Montero <eduardo.canaljunior@gmail.com>
2021-05-14 12:28:41 -0300

    update listening port

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(`The application is listening on port ${PORT}!`);
 })
